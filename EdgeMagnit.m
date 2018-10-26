07c85eace5f1ca9ea5452945286d6ff2fc4ea810
Triplem5ds <mmmanany@yahoo.com>
2018-10-26 14:14:35 +0200

    tslee7 el push el awlany

--- EdgeMagnit.m
+++ EdgeMagnit.m
@@ -2,10 +2,5 @@ function out = EdgeMagnit(I)
   X = Apply(I,Sobel('H'),'Absluote');
   Y = Apply(I,Sobel('V'),'Absluote');
   out = X+Y;
-  [n, m] = size(out)
-  for i=1:n
-    for j=1:m
-      out(i,j) = min(out(i,j),255);
-    endfor
-  endfor
+  out = Postprocessing(out, 'cutoff');	
 endfunction
